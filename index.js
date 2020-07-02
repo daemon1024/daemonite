2aacf6e53403fa8ba41ade3f3067d1af87fb00d2
daemon1024 <barun1024@gmail.com>
2020-07-02 16:04:38 +0530

    "add" function

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const { emitter } = require("./emitter");
 
 const main = async () => {
   const wasm = emitter();
-  const instance = await WebAssembly.instantiate(wasm);
+  const { instance } = await WebAssembly.instantiate(wasm);
+  console.log(instance.exports.run(5, 6));
 };
+
+main();
